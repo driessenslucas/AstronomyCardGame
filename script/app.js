a66bc1dff00e2e4a76c3b3a290517ddfdb7a4f32
lucasdriessens <lucas.driessens@student.howest.be>
2022-11-04 13:35:06 +0100

    na flip card animation

--- script/app.js
+++ script/app.js
@@ -1,17 +1,36 @@
 let apiKey = '0wbfxSQ6gzlHNFmELg7orwZgf5cFqVzFALmBrvzH';
+let ListjsonObjects;
 
-const showBigImg = function (url, title) {
-	document.querySelector(
-		'.c-bigpic'
-	).innerHTML = `<h1 class="c-title"> ${title}</h1> <img class="c-img 	" src="${url}" alt="" />`;
+const recentlyDeleted = function (jsonObject) {
+	console.log(jsonObject);
+	document.querySelector('.c-rect1').innerHTML = `<image
+	
+	href="${jsonObject.url}"
+	width="1210"
+	height="613"
+/>`;
 };
 
-const showData2 = function (jsonObject) {
+const showBigImg = function (url, title) {
+	// document.querySelector(
+	// 	'.c-bigpic'
+	// ).innerHTML = `<h1 class="c-title"> ${title}</h1> <img class="c-img 	" src="${url}" alt="" />`;
+	document.querySelector('.c-imgurl').innerHTML = `<image
+	
+	href="${url}"
+	width="1210"
+	height="613"
+/>`;
+};
+
+//maak tweede showdata enkel voor eerste keer inladen
+
+const showData2 = async function (jsonObject) {
 	console.log(jsonObject);
 	let html = '';
 	for (let i = 0; i < jsonObject.length; i++) {
 		if (jsonObject[i].media_type == 'image') {
-			html += `<div class="card-container">
+			html += `<div class="card-container c-hidden flipped">
 		<div class="card cardnr${i}">
 			<div class="front">
 			<img class="c-img c-imgnr${i}" src="${jsonObject[i].url}" alt="${jsonObject[i].title}" />
@@ -39,72 +58,32 @@ const showData2 = function (jsonObject) {
 		}
 	}
 	document.querySelector('.c-cards').innerHTML = html;
+	let cards = document.querySelectorAll('.card-container');
+
+	cards[0].classList.remove('c-hidden');
+	await new Promise((resolve) => setTimeout(resolve, 350)); // 3 sec
+
+	cards[0].classList.remove('flipped');
+	await new Promise((resolve) => setTimeout(resolve, 900)); // 3 sec
+
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].classList.remove('c-hidden');
+		cards[i].classList.remove('flipped');
+	}
+
+	// document.querySelectorAll('.card-container').forEach((card) => {
+	// 	card.classList.remove('c-hidden');
+	// 	card.classList.remove('flipped');
+	// });
 	listenToClick();
 };
 
-// const showData = function (jsonObject) {
-// 	console.log(jsonObject);
-
-// 	let html = '';
-// 	let i = 0;
-// 	for (const item in jsonObject) {
-// 		console.log(jsonObject[i]);
-// 		html += `<div class="c-picdiv">
-//             <h1 class="c-title">${jsonObject[i].title}</h1>
-//             <div class="c-APOD"><img class="c-img js-imgOTD js-imgOTD${i}" src="${jsonObject[i].url}" alt="" /></div>
-//             <div class="c-popup-box c-hidden c-discription${i}">
-//                 <div class="o-layout">
-//                     <div class="c-closebtn o-layout__item u-1-of-2">
-//                         <svg
-//                             xmlns="http://www.w3.org/2000/svg"
-//                             height="48"
-//                             width="48"
-//                         >
-//                             <path
-//                                 d="M24 38 12 26l2.1-2.1 9.9 9.9 9.9-9.9L36 26Zm0-12.65-12-12 2.1-2.1 9.9 9.9 9.9-9.9 2.1 2.1Z"
-//                             />
-//                         </svg>
-//                     </div>
-//                     <div class="c-authorbox o-layout__item u-1-of-2">
-//                         <h2 class="u-text-xl">Author:</h2>
-//                     </div>
-//                 </div>
-//                 <div class="c-discriptionbox ">
-
-//                     <h2 class="c-author">${jsonObject[i].copyright}</h2>
-//                     <h2 class="u-text-xl">discription:</h2>
-//                     <div class="c-discription"><div class="u-text-sm" > ${jsonObject[i].explanation}</div></div>
-//                 </div>
-//             </div>
-//             <div class="c-openbtn c-btnnr${i}">
-//                 <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-//                     <path
-//                         d="m14.1 36.75-2.1-2.1 12-12 12 12-2.1 2.1-9.9-9.9Zm0-12.65L12 22l12-12 12 12-2.1 2.1-9.9-9.9Z"
-//                     />
-//                 </svg>
-//             </div>
-//         </div>`;
-// 		i++;
-// 	}
-// 	document.querySelector('.c-mainbody').innerHTML = html;
-// 	listenToClick();
-
-// 	// document.querySelector('.c-title').innerHTML = jsonObject.title;
-
-// 	// document.querySelector(
-// 	// 	'.c-APOD'
-// 	// ).innerHTML = `<img class="c-img js-imgOTD" src="${jsonObject.hdurl}" alt="" />`;
-
-// 	// document.querySelector(
-// 	// 	'.c-discription'
-// 	// ).innerHTML = `<div class="u-text-sm" > ${jsonObject.explanation}</div>`;
-
-// 	// document.querySelector('.c-author').innerHTML = jsonObject.copyright;
-// };
-
 const getApi = function () {
 	let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=8`;
-	handleData(url, showData2);
+	handleData(url, function (jsonObject) {
+		ListjsonObjects = jsonObject;
+		showData2(ListjsonObjects);
+	});
 };
 
 const listenToClick = function () {
@@ -117,41 +96,24 @@ const listenToClick = function () {
 				document.querySelector(`${imgnr}`).src,
 				document.querySelector(`${imgnr}`).alt
 			);
+
+			//delete jsonobject with image url
+
+			recentlyDeleted(ListjsonObjects[i]);
+			ListjsonObjects.splice(i, 1);
+			console.log(ListjsonObjects);
+			showData2(ListjsonObjects);
 		});
 	}
+};
 
-	// document
-	// 	.querySelector('.c-openbtn')
-	// 	.addEventListener('click', function () {});
-	// document
-	// 	.querySelector('.c-closebtn')
-	// 	.addEventListener('click', function () {});
-	// const openbtns = document.querySelectorAll('.c-openbtn');
-	// console.log(openbtns.length);
-	// for (let i = 0; i < openbtns.length; i++) {
-	// 	openbtns[i].addEventListener('click', function (e) {
-	// 		let boxnr = `.c-discription${i}`;
-	// 		let btnnr = `.c-btnnr${i}`;
-	// 		let imgnr = `.js-imgOTD${i}`;
-	// 		document.querySelector(`${boxnr}`).classList.remove('c-hidden');
-	// 		console.log(boxnr);
-	// 		document.querySelector(`${btnnr}`).classList.add('c-hidden');
-	// 		showBigImg(document.querySelector(`${imgnr}`).src);
-	// 	});
-	// }
-	// const closedbtns = document.querySelectorAll('.c-closebtn');
-	// for (let i = 0; i < closedbtns.length; i++) {
-	// 	closedbtns[i].addEventListener('click', function (e) {
-	// 		let boxnr = `.c-discription${i}`;
-	// 		let btnnr = `.c-btnnr${i}`;
-	// 		document.querySelector(`${boxnr}`).classList.add('c-hidden');
-	// 		document.querySelector(`${btnnr}`).classList.remove('c-hidden');
-	// 		showBigImg('');
-	// 	});
-	// }
+const listenser = function () {
+	cards = document.querySelectorAll('.c-card');
 };
 
 document.addEventListener('DOMContentLoaded', function () {
+	listenser();
+
 	// document.querySelector('.c-card').addEventListener('click', function () {
 	// 	document.querySelector('.c-card').classList.toggle('flipped');
 	// });
