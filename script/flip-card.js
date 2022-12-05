12b78b9a01b4a60295ebc21c2fab1d9d627de495
lucasdriessens <lucas.driessens@student.howest.be>
2022-12-05 11:16:14 +0100

    fixed: could select same card twice + fixed: card positions

--- script/flip-card.js
+++ script/flip-card.js
@@ -7,39 +7,38 @@ let nummerOfCardNotFlipped = 0;
 let ChosenCards = [];
 let cardnr = 0;
 
-function shuffleArray(array) {
+const shuffleArray = (array) => {
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
-}
-
+};
 const showData = function (jsonObject) {
+	console.log('ok');
 	shuffleArray(CardsArray);
 	let imgnr = 1;
-	for (let i = 0; i < CardsArray[0].length; i++) {
-		for (let x = 0; x < CardsArray[1].length; x++) {
-			console.log(CardsArray);
-			html += `
+	for (let i = 0; i < CardsArray.length; i++) {
+		html += `
         <div class="c-playboard__item cardnr${imgnr}"><div class="card-container c-card flipped">
 			<div class="card">
 				<div class="front">
-                <img class="c-img c-imgnr${imgnr}" src="${CardsArray[i][x].url}" alt="${CardsArray[i][x].title}" />
+                <img class="c-img c-imgnr${imgnr}" src="${CardsArray[i].url}" alt="${CardsArray[i].title}" />
 					
 				</div>
 				<div class="back">
-					<svg viewBox="0 0 300 420">
+					<svg class="card-svg" viewBox="0 0 300 420">
 						<use xlink:href="#card-back" />
 					</svg>
 				</div>
 			</div>
 		</div></div>`;
-			imgnr++;
-		}
-
-		document.querySelector('.c-playboard').innerHTML = html;
-		listenToClick();
+		imgnr++;
 	}
+
+	document.querySelector('.c-playboard').innerHTML = html;
+	listenToClick();
 };
 
 const checkCardsInHand = function () {
@@ -51,16 +50,16 @@ const checkCardsInHand = function () {
 			}
 		}
 		if (cards[0]) {
-			cards[0].classList.add('card1');
+			cards[0].classList.add('card3');
 		}
 		if (cards[1]) {
-			cards[1].classList.add('card2');
+			cards[1].classList.add('card4');
 		}
 		if (cards[2]) {
-			cards[2].classList.add('card3');
+			cards[2].classList.add('card5');
 		}
 		if (cards[3]) {
-			cards[3].classList.add('card4');
+			cards[3].classList.add('card6');
 		}
 		if (cards[4]) {
 			cards[4].classList.add('card5');
@@ -81,56 +80,45 @@ const addToHand = async (ChosenCard) => {
 	console.log(ChosenCard);
 	let handHtml = '';
 	let mobilenavHtml = '';
-	for (let i = 0; i < ChosenCard.length; i++) {
-		if (ChosenCard[i].media_type === 'image') {
-			handHtml += `<div class="card-container2 ">
+	handHtml += `<div class="card-container2 ">
 			<div class="card cardnr${cardnr}">
 				<div class="front">
-					<img class="c-img c-imgnr${cardnr}" src="${ChosenCard[i].url}" alt="${ChosenCard[i].title}" />
+					<img class="c-img c-imgnr${cardnr}" src="${ChosenCard.url}" alt="${ChosenCard.title}" />
 					</div>
 					<div class="back">
-						<svg viewBox="0 0 300 420">
+						<svg class="card-svg" viewBox="0 0 300 420">
 							<use xlink:href="#card-back" />
 						</svg>
 					</div>
 				</div>
 			</div>`;
-			mobilenavHtml += `<div class="c-mobile-nav__item ">
-				<img class="c-nav__img c-imgnr${cardnr}" src="${ChosenCard[i].url}" alt="${ChosenCard[i].title}" />
-				<h2 class="c-nav__item-title">${ChosenCard[i].title}</h2>
+	mobilenavHtml += `<div class="c-mobile-nav__item ">
+				<img class="c-nav__img c-imgnr${cardnr}" src="${ChosenCard.url}" alt="${ChosenCard.title}" />
+				<h2 class="c-nav__item-title">${ChosenCard.title}</h2>
 			</div>`;
-		}
-		cardnr++;
-	}
+
 	document.querySelector('.c-card__nav').innerHTML += mobilenavHtml;
 	document.querySelector('.c-cards').innerHTML += handHtml;
-	// await new Promise((r) => setTimeout(r, 1000));
-	// document.querySelectorAll('.card-container2').forEach((card) => {
-	// 	card.classList.remove('c-hidden');
-	// });
+
 	checkCardsInHand();
 };
 
-const checkCards = (card1, card2) => {
+const checkCards = async (card1, card2) => {
 	console.log(card1);
 	console.log(card2);
-
 	const cards = document.querySelectorAll('.card-container');
-	if (card1[0].url === card2[0].url) {
-		addToHand(ChosenCards[1]);
+	if (card1.url == card2.url && ChosenCardnrs[0] != ChosenCardnrs[1]) {
+		console.log('gelijk');
+		console.log(ChosenCards);
+		addToHand(ChosenCards[0]);
 		console.log(ChosenCardnrs);
-		// document
-		// 	.querySelector('.c-playboard')
-		// 	.removeChild(document.querySelector(ChosenCardnrs[0]));
-		// document
-		// 	.querySelector('.c-playboard')
-		// 	.removeChild(document.querySelector(ChosenCardnrs[1]));
 		document.querySelector(ChosenCardnrs[1]).classList.add('c-hide');
 		document.querySelector(ChosenCardnrs[0]).classList.add('c-hide');
 		ChosenCardnrs = [];
 		nummerOfCardNotFlipped = 0;
 		ChosenCards = [];
 	} else {
+		console.log('niet gelijk');
 		cards.forEach((card) => {
 			card.classList.add('flipped');
 			ChosenCards = [];
@@ -144,20 +132,21 @@ const listenToClick = () => {
 	const cards = document.querySelectorAll('.card-container');
 	for (let i = 0; i < cards.length; i++) {
 		cards[i].addEventListener('click', async (e) => {
-			nummerOfCardNotFlipped++;
-			console.log(nummerOfCardNotFlipped);
 			if (nummerOfCardNotFlipped <= 2) {
+				console.log(ChosenCardnrs);
+				let nr;
 				cards[i].classList.remove('flipped');
-				let nr = i + 1;
+				nummerOfCardNotFlipped++;
+				nr = i + 1;
 				ChosenCardnrs.push(`.cardnr${nr}`);
 				let imgnr = `.c-imgnr${nr}`;
-				const result = CardsArray[0].filter(
+				const result = CardsArray.filter(
 					(word) => word.url === document.querySelector(`${imgnr}`).src
 				);
-				ChosenCards.push(result);
+				ChosenCards.push(result[0]);
 				await new Promise((resolve) => setTimeout(resolve, 1000));
 				if (nummerOfCardNotFlipped == 2) {
-					checkCards(ChosenCards[0], ChosenCards[1]);
+					await checkCards(ChosenCards[0], ChosenCards[1]);
 					nummerOfCardNotFlipped = 0;
 				}
 			}
@@ -175,7 +164,8 @@ const getApi = async function () {
 		array.push(jsonObject);
 		let array2 = [];
 		array2.push(jsonObject);
-		CardsArray = [...array, ...array2];
+		CardsArray = array[0].concat(array2[0]);
+
 		showData(jsonObject);
 	});
 };
@@ -186,9 +176,20 @@ const listenToBlur = () => {
 	});
 };
 
+function toggleNav() {
+	document
+		.querySelector('.js-toggle-nav')
+		.addEventListener('click', function () {
+			document.querySelector('body').classList.toggle('has-mobile-nav');
+
+			document.querySelector('.c-mobile-nav').classList.add('c-hidden');
+			document.querySelector('.c-blur').classList.add('c-hidden');
+		});
+}
+
 const init = () => {
 	getApi();
-	if (document.querySelector('.hasmobile-nav')) {
+	if (document.querySelector('.has-mobile-nav')) {
 		listenToBlur();
 	}
 };
@@ -196,4 +197,5 @@ const init = () => {
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('dom content loaded');
 	init();
+	toggleNav();
 });
