6d0fd31e0bca736afb134fe0d31e533bb97bf92c
lucasdriessens <lucas.driessens@student.howest.be>
2022-12-05 12:28:05 +0100

    added: show bigcard

--- script/flip-card.js
+++ script/flip-card.js
@@ -4,7 +4,7 @@ let html = '';
 let ChosenCardnrs = [];
 let nummerOfCardNotFlipped = 0;
 let ChosenCards = [];
-let cardnr = 0;
+let cardnr = 1;
 
 const shuffleArray = (array) => {
 	for (let i = array.length - 1; i > 0; i--) {
@@ -43,6 +43,26 @@ const showData = function (jsonObject) {
 	const { forceGridAnimation } = animateCSSGrid.wrapGrid(grid);
 
 	forceGridAnimation();
+};
+
+const showBigCard = async function (jsonObject) {
+	console.log(jsonObject);
+	let html = `<div class="o-button-reset c-nav-trigger js-toggle-nav js-close"><svg
+					class="c-nav-trigger__svg"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg">
+					<path
+								d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+							/>
+				</svg></div>
+				<h2 class="c-title u-text-xl">${jsonObject[0].title}</h2>
+        <img src="${jsonObject[0].url}" alt="" class="c-APOD" />
+        <p class="c-discription u-text-rg">${jsonObject[0].explanation}</p>`;
+	// document.querySelector('.c-Date').innerHTML = jsonObject.date;
+	document.querySelector('.c-bigCard').innerHTML = html;
+	document.querySelector('.c-bigCard').classList.remove('c-hidden');
+
+	listenToClose();
 };
 
 const checkCardsInHand = function () {
@@ -84,6 +104,7 @@ const addToHand = async (ChosenCard) => {
 	console.log(ChosenCard);
 	let handHtml = '';
 	let mobilenavHtml = '';
+
 	handHtml += `<div class="card-container2 ">
 			<div class="card cardnr${cardnr}">
 				<div class="front">
@@ -100,11 +121,13 @@ const addToHand = async (ChosenCard) => {
 				<img class="c-nav__img c-imgnr${cardnr}" src="${ChosenCard.url}" alt="${ChosenCard.title}" />
 				<h2 class="c-nav__item-title">${ChosenCard.title}</h2>
 			</div>`;
-
+	cardnr++;
 	document.querySelector('.c-card__nav').innerHTML += mobilenavHtml;
 	document.querySelector('.c-cards').innerHTML += handHtml;
 
 	checkCardsInHand();
+	listenToClickHand();
+	listenToClickHandMobile();
 };
 
 const checkCards = async (card1, card2) => {
@@ -118,21 +141,22 @@ const checkCards = async (card1, card2) => {
 		console.log(ChosenCardnrs);
 		// document.querySelector(ChosenCardnrs[1]).classList.add('c-hide');
 		// document.querySelector(ChosenCardnrs[0]).classList.add('c-hide');
-
+		await new Promise((resolve) => setTimeout(resolve, 100));
 		document
 			.querySelector('.c-playboard')
 			.removeChild(document.querySelector(ChosenCardnrs[0]));
 		document
 			.querySelector('.c-playboard')
 			.removeChild(document.querySelector(ChosenCardnrs[1]));
-		await new Promise((resolve) => setTimeout(resolve, 500));
+		await new Promise((resolve) => setTimeout(resolve, 300));
 		addToHand(ChosenCards[0]);
 		ChosenCardnrs = [];
 		nummerOfCardNotFlipped = 0;
 		ChosenCards = [];
 	} else {
 		console.log('niet gelijk');
-		cards.forEach((card) => {
+		cards.forEach(async (card) => {
+			await new Promise((resolve) => setTimeout(resolve, 600));
 			card.classList.add('flipped');
 			ChosenCards = [];
 			ChosenCardnrs = [];
@@ -169,6 +193,50 @@ const listenToClick = () => {
 		});
 	}
 };
+const listenToClickHand = () => {
+	const cardsInHand = document.querySelectorAll('.card-container2');
+	cardsInHand.forEach((element) => {
+		for (let index = 0; index < cardsInHand.length; index++) {
+			cardsInHand[index].addEventListener('click', () => {
+				let nr = index + 1;
+				let imgnr = `.c-imgnr${nr}`;
+				console.log('clicked card');
+				const result = CardsArray.filter(
+					(word) => word.url === document.querySelector(`${imgnr}`).src
+				);
+				showBigCard(result);
+			});
+		}
+	});
+};
+
+const listenToClickHandMobile = () => {
+	const cardsInHand = document.querySelectorAll('.c-mobile-nav__item');
+	console.log(cardsInHand);
+	cardsInHand.forEach((element) => {
+		for (let index = 0; index < cardsInHand.length; index++) {
+			cardsInHand[index].addEventListener('click', () => {
+				document.querySelector('body').classList.toggle('has-mobile-nav');
+				document.querySelector('.c-mobile-nav').classList.toggle('c-hidden');
+				document.querySelector('.c-blur').classList.toggle('c-hidden');
+				let nr = index + 1;
+				let imgnr = `.c-imgnr${nr}`;
+				console.log('clicked card');
+				const result = CardsArray.filter(
+					(word) => word.url === document.querySelector(`${imgnr}`).src
+				);
+				showBigCard(result);
+			});
+		}
+	});
+};
+
+const listenToClose = () => {
+	document.querySelector('.js-close').addEventListener('click', function () {
+		console.log('click');
+		document.querySelector('.c-bigCard').classList.add('c-hidden');
+	});
+};
 
 const getApi = async function () {
 	let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=4`;
@@ -178,7 +246,6 @@ const getApi = async function () {
 		let array2 = [];
 		array2.push(jsonObject);
 		CardsArray = array[0].concat(array2[0]);
-
 		showData(jsonObject);
 	});
 };
@@ -191,18 +258,15 @@ const listenToBlur = () => {
 };
 
 function toggleNav() {
-	document.querySelector('.js-open').addEventListener('click', function () {
-		console.log('click');
-		document.querySelector('body').classList.toggle('has-mobile-nav');
-		document.querySelector('.c-mobile-nav').classList.toggle('c-hidden');
-		document.querySelector('.c-blur').classList.toggle('c-hidden');
+	document.querySelectorAll('.js-toggle-nav').forEach((element) => {
+		element.addEventListener('click', function () {
+			console.log('click');
+			document.querySelector('body').classList.toggle('has-mobile-nav');
+			document.querySelector('.c-mobile-nav').classList.toggle('c-hidden');
+			document.querySelector('.c-blur').classList.toggle('c-hidden');
+		});
 	});
-	document.querySelector('.js-close').addEventListener('click', function () {
-		console.log('click');
-		document.querySelector('body').classList.toggle('has-mobile-nav');
-		document.querySelector('.c-mobile-nav').classList.toggle('c-hidden');
-		document.querySelector('.c-blur').classList.toggle('c-hidden');
-	});
+
 	listenToBlur();
 }
 
